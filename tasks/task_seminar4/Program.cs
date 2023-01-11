320623a0ac7818815d13a94af8ebc1f22bff3cc9
Evgeniya <evgeniya26.12@mail.ru>
2023-01-11 22:57:57 +0300

    new

---
+++ tasks/task_seminar4/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+// тип итоговой переменной + Название метода+()+
+// {
+// return sum;
+// }
+
+
+
+
+int GetSumN(int numString)
+{
+    int sum = 0;
+    while (numString>0)
+    {
+        sum += numString % 10;
+        numString /= 10;
+    }
+    return sum;
+    }
+
+
+Console.WriteLine("введите число N");
+int n = Convert.ToInt32(Console.ReadLine());
+
+
+Console.WriteLine(GetSumN(n));
