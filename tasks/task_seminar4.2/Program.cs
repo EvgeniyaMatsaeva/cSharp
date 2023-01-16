be1c4aa66635411086821f277623cf25e29b002f
Evgeniya <evgeniya26.12@mail.ru>
2023-01-16 16:04:26 +0300

    создала 2 массива и 1 метод заполнения

--- tasks/task_seminar4.2/Program.cs
+++ tasks/task_seminar4.2/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+
+
+    
+Console.WriteLine("введите число A");
+int A = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("введите число B");
+int B = Convert.ToInt32(Console.ReadLine());
+
+int GetNun()
+    {
+       int result = 1;
+        for(int i = 1; i <= B ; i++)
+    {
+        result = result * A;
+    }
+    return result;
+}
+int result = GetNun();
+Console.WriteLine(result);
+
